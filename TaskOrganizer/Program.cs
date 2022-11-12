1a71d4006ab03052eef5474d8a926ce0ce6e76d8
buddikatdk <49783759+buddikatdk@users.noreply.github.com>
2022-11-12 12:35:03 +0530

    Add project files.

---
+++ TaskOrganizer/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please Enter Starting date (format should be as MM/dd/yyyy)");
+DateTime date = Convert.ToDateTime(Console.ReadLine());
+
+Console.WriteLine("Please Enter Task effort(days requried)");
+int dayCount = Convert.ToInt32(Console.ReadLine());
+
+
+//check start day is holiday or not
+if(date.DayOfWeek == DayOfWeek.Sunday || date.DayOfWeek == DayOfWeek.Saturday || date.)
+{
+    Console.WriteLine("Starting date is not a week day.it's " + date.DayOfWeek + " day");
+}
+
+while(dayCount >= 0)
+{
+
+
+
+
+    dayCount--;
+}
+
+Console.WriteLine(date);
