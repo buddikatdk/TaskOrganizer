43f481bcb0230174521575b02993be8d01ab1db7
buddikatdk <49783759+buddikatdk@users.noreply.github.com>
2022-11-12 12:39:37 +0530

    Added special holiday class to hold list of specified holidays

---
+++ TaskOrganizer/Utility/SpecialHolidays.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskOrganizer.Utility
+{
+    public class SpecialHolidays
+    {
+        List<string> specifiedHoliday = new List<string>() {
+            "2022-11-12 : Poya day",
+        };
+    }
+}
